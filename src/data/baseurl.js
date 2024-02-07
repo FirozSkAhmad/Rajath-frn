775dc36b0a300d504bb49f6c8973e82332fab91c
Firoz shaik <shaikfirozahmad99@gmail.com>
2024-02-07 12:45:43 +0530

    changed the base url

--- src/data/baseurl.js
+++ src/data/baseurl.js
@@ -1,7 +1,7 @@
 const BASEURL = {
-    // 'url': 'https://pab-backend.onrender.com',
-    'url': 'http://localhost:5200',
-    'token': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDU3NjQxMjEsImV4cCI6MTczNzMyMTcyMSwiYXVkIjoiMTpTVVBFUiBBRE1JTiIsImlzcyI6InBhYmFwcGxpY2F0aW9uIn0.4dkblN2KV86c5MQu85PnFsnDVq1j9uTvmh5tJ9x1YA4'
+    'url': 'https://pab-backend.onrender.com',
+    // 'url': 'http://localhost:5200',
+    // 'token': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDU3NjQxMjEsImV4cCI6MTczNzMyMTcyMSwiYXVkIjoiMTpTVVBFUiBBRE1JTiIsImlzcyI6InBhYmFwcGxpY2F0aW9uIn0.4dkblN2KV86c5MQu85PnFsnDVq1j9uTvmh5tJ9x1YA4'
 }
 
 export default BASEURL;
